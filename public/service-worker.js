bb7b3b4e495effee3c8d1a41c725f9b07ed4b540
Maryam Chaudhry <maryamc1798@gmail.com>
2021-02-23 00:17:07 -0500

    made some changes to service worker app is working but breaks after a few seconds so idk

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,11 @@ console.log("Hello from your service worker!");
 
 const FILES_TO_CACHE = [
     "/",
+    "/index.js",
     "/index.html",
+    "/db.js",
     "/styles.css",
     "/manifest.webmanifest",
-    "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
 ];
